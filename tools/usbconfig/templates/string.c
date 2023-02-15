5d5bc2dc56c4e8e7d32ac62fee9fee0c8fed4773
mikee47 <mike@silyhouse.net>
2023-02-15 19:34:15 +0000

    WIP

--- tools/usbconfig/templates/string.c
+++ tools/usbconfig/templates/string.c
@@ -5,7 +5,7 @@
 // array of pointer to string descriptors, first element is the string length
 static const char* string_desc_arr [] =
 {
-  "\x02\x09\x04", // 0: is supported language is English (0x0409)
+  "\x01\x09\x04", // 0: is supported language is English (0x0409)
 ${strings}};
 
 // Invoked when received GET STRING DESCRIPTOR request
@@ -20,24 +20,21 @@ const uint16_t* tud_descriptor_string_cb(uint8_t index, uint16_t langid)
     uint16_t str[${max_string_len}];
   };
   StringBuffer buf;
-  uint8_t chr_count;
 
+  // Note: the 0xEE index string is a Microsoft OS 1.0 Descriptors.
+  // https://docs.microsoft.com/en-us/windows-hardware/drivers/usbcon/microsoft-defined-usb-descriptors
+
+  if(index >= TU_ARRAY_SIZE(string_desc_arr)) {
+    return NULL;
+  }
+
+  const char* str = string_desc_arr[index];
+  uint8_t chr_count = *str++;
   if (index == 0)
   {
-    memcpy(buf.str, string_desc_arr[0], 2);
-    chr_count = 1;
+    memcpy(buf.str, str, 2);
   } else {
-    // Note: the 0xEE index string is a Microsoft OS 1.0 Descriptors.
-    // https://docs.microsoft.com/en-us/windows-hardware/drivers/usbcon/microsoft-defined-usb-descriptors
-
-    if(index >= TU_ARRAY_SIZE(string_desc_arr)) {
-      return NULL;
-    }
-
-    const char* str = string_desc_arr[index];
-
     // Convert ASCII string into UTF-16
-    chr_count = *str++;
     for(unsigned i=0; i<chr_count; ++i) {
       buf.str[i] = str[i];
     }
