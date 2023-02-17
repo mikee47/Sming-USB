ecc159e92a5c89997f84724c19432ddec7f389a8
mikee47 <mike@silyhouse.net>
2023-02-17 11:09:53 +0000

    Testing CDC behaviour

--- samples/Composite/app/application.cpp
+++ samples/Composite/app/application.cpp
@@ -166,6 +166,72 @@ int32_t tud_msc_write10_cb(uint8_t lun, uint32_t lba, uint32_t offset, uint8_t* 
 	return -1;
 }
 
+static String cdcBuffer;
+
+// Invoked when received new data
+void tud_cdc_rx_cb(uint8_t itf)
+{
+	FUNC()
+	debug_i("cdc itf = %u", itf);
+
+	char buf[64];
+	auto count = tud_cdc_n_read(itf, buf, sizeof(buf));
+	debug_hex(INFO, "CDC", buf, count);
+
+	cdcBuffer.concat(buf, count);
+
+	auto written = tud_cdc_n_write(0, cdcBuffer.c_str(), cdcBuffer.length());
+	debug_i("tud_cdc_n_write: %u", written);
+	cdcBuffer.remove(0, written);
+	tud_cdc_n_write_flush(0);
+}
+
+// Invoked when received `wanted_char`
+void tud_cdc_rx_wanted_cb(uint8_t itf, char wanted_char)
+{
+	FUNC()
+
+
+}
+
+// Invoked when a TX is complete and therefore space becomes available in TX buffer
+void tud_cdc_tx_complete_cb(uint8_t itf)
+{
+	FUNC()
+
+	if(cdcBuffer.length() == 0) {
+		return;
+	}
+	auto written = tud_cdc_n_write(0, cdcBuffer.c_str(), cdcBuffer.length());
+	debug_i("tud_cdc_n_write: %u", written);
+	cdcBuffer.remove(0, written);
+	tud_cdc_n_write_flush(0);
+}
+
+// Invoked when line state DTR & RTS are changed via SET_CONTROL_LINE_STATE
+void tud_cdc_line_state_cb(uint8_t itf, bool dtr, bool rts)
+{
+	FUNC()
+
+	debug_i("DTR %u, RTS %u", dtr, rts);
+}
+
+// Invoked when line coding is change via SET_LINE_CODING
+void tud_cdc_line_coding_cb(uint8_t itf, cdc_line_coding_t const* p_line_coding)
+{
+	FUNC()
+
+
+}
+
+// Invoked when received send break
+void tud_cdc_send_break_cb(uint8_t itf, uint16_t duration_ms)
+{
+	FUNC()
+
+
+}
+
 void init()
 {
 	Serial.begin(SERIAL_BAUD_RATE);
