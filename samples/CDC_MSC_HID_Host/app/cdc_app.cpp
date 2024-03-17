9f16a5b748e98a3d969a08fa729ec3205b59a6a9
mikee47 <mike@silyhouse.net>
2024-03-17 12:17:46 +0000

    Update to tinyusb master

--- samples/CDC_MSC_HID_Host/app/cdc_app.cpp
+++ samples/CDC_MSC_HID_Host/app/cdc_app.cpp
@@ -57,10 +57,10 @@ void tuh_cdc_rx_cb(uint8_t idx)
 
 void tuh_cdc_mount_cb(uint8_t idx)
 {
-	tuh_cdc_itf_info_t itf_info = {0};
+	tuh_itf_info_t itf_info{};
 	tuh_cdc_itf_get_info(idx, &itf_info);
 
-	printf("CDC Interface is mounted: address = %u, itf_num = %u\r\n", itf_info.daddr, itf_info.bInterfaceNumber);
+	debug_i("CDC Interface is mounted: address = %u, itf_num = %u", itf_info.daddr, itf_info.desc.bInterfaceNumber);
 
 #ifdef CFG_TUH_CDC_LINE_CODING_ON_ENUM
 	// CFG_TUH_CDC_LINE_CODING_ON_ENUM must be defined for line coding is set by tinyusb in enumeration
@@ -75,8 +75,8 @@ void tuh_cdc_mount_cb(uint8_t idx)
 
 void tuh_cdc_umount_cb(uint8_t idx)
 {
-	tuh_cdc_itf_info_t itf_info{};
+	tuh_itf_info_t itf_info{};
 	tuh_cdc_itf_get_info(idx, &itf_info);
 
-	debug_i("CDC Interface is unmounted: address = %u, itf_num = %u", itf_info.daddr, itf_info.bInterfaceNumber);
+	debug_i("CDC Interface is unmounted: address = %u, itf_num = %u", itf_info.daddr, itf_info.desc.bInterfaceNumber);
 }
