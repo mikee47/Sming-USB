d725b6ba112f9c30ab04adf9bb0981364185c1a3
mikee47 <mike@silyhouse.net>
2024-03-17 19:46:26 +0000

    Enable python UTF8 mode for usbconfig tool

--- component.mk
+++ component.mk
@@ -38,7 +38,7 @@ GLOBAL_CFLAGS += \
 COMPONENT_VARS += USB_CONFIG
 ifdef USB_CONFIG
 
-USBCONFIG_TOOL := $(PYTHON) $(COMPONENT_PATH)/tools/usbconfig/usbconfig.py
+USBCONFIG_TOOL := $(PYTHON) -Xutf8 $(COMPONENT_PATH)/tools/usbconfig/usbconfig.py
 
 USB_OUTPUT_DIR := $(PROJECT_DIR)/$(BUILD_BASE)/USB
 USB_CONFIG_H := $(USB_OUTPUT_DIR)/tusb_config.h
