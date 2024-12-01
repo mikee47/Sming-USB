93df33e5bc746e429de942f7330a45a4395c41b9
mikee47 <mike@sillyhouse.net>
2024-12-01 11:36:09 +0000

    Use IDF HAL to initialise PHY

--- src/Arch/Esp32/init.cpp
+++ src/Arch/Esp32/init.cpp
@@ -20,66 +20,27 @@
  ****/
 
 #include <USB.h>
-
-#include <esp_rom_gpio.h>
-#include <hal/gpio_ll.h>
-#include <soc/periph_defs.h>
-#include <driver/periph_ctrl.h>
-
-#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 2, 2)
-#include <soc/usb_periph.h>
-#include <soc/usb_reg.h>
-#include <hal/usb_wrap_ll.h>
-#elif ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 2, 0)
-#include <soc/usb_periph.h>
-#include <soc/usb_reg.h>
-#include <hal/usb_fsls_phy_hal.h>
-#include <hal/usb_fsls_phy_ll.h>
-#else
-#include <hal/usb_ll.h>
-#endif
-
-namespace
-{
-void configure_pins()
-{
-	for(auto iopin = usb_periph_iopins; iopin->pin >= 0; ++iopin) {
-		if(iopin->ext_phy_only) {
-			continue;
-		}
-		esp_rom_gpio_pad_select_gpio(iopin->pin);
-		if(iopin->is_output) {
-			esp_rom_gpio_connect_out_signal(iopin->pin, iopin->func, false, false);
-		} else {
-			esp_rom_gpio_connect_in_signal(iopin->pin, iopin->func, false);
-			if((iopin->pin != GPIO_MATRIX_CONST_ZERO_INPUT) && (iopin->pin != GPIO_MATRIX_CONST_ONE_INPUT)) {
-				gpio_ll_input_enable(&GPIO, gpio_num_t(iopin->pin));
-			}
-		}
-		esp_rom_gpio_pad_unhold(iopin->pin);
-	}
-	gpio_ll_set_drive_capability(&GPIO, gpio_num_t(USBPHY_DM_NUM), GPIO_DRIVE_CAP_3);
-	gpio_ll_set_drive_capability(&GPIO, gpio_num_t(USBPHY_DM_NUM), GPIO_DRIVE_CAP_3);
-}
-
-} // namespace
+#include <esp_err.h>
+#include <esp_private/usb_phy.h>
 
 namespace USB
 {
 void initHardware()
 {
-	periph_module_reset(PERIPH_USB_MODULE);
-	periph_module_enable(PERIPH_USB_MODULE);
-#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 2, 2)
-	usb_wrap_ll_phy_enable_external(&USB_WRAP, false);
-	usb_wrap_ll_phy_enable_pad(&USB_WRAP, true);
-#elif ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 2, 0)
-	usb_fsls_phy_ll_usb_wrap_pad_enable(&USB_WRAP, true);
-	usb_fsls_phy_ll_int_otg_enable(&USB_WRAP);
-#else
-	usb_ll_int_phy_enable();
+	usb_phy_config_t phy_conf = {
+		.controller = USB_PHY_CTRL_OTG,
+		.target = USB_PHY_TARGET_INT,
+#if CFG_TUD_ENABLED
+		.otg_mode = USB_OTG_MODE_DEVICE,
+		.otg_speed = BOARD_TUD_RHPORT ? USB_PHY_SPEED_HIGH : USB_PHY_SPEED_FULL,
+#elif CFG_TUH_ENABLED
+		.otg_mode = USB_OTG_MODE_HOST,
+		.otg_speed = BOARD_TUH_RHPORT ? USB_PHY_SPEED_HIGH : USB_PHY_SPEED_FULL,
 #endif
-	configure_pins();
+	};
+
+	usb_phy_handle_t phy_hdl;
+	usb_new_phy(&phy_conf, &phy_hdl);
 }
 
 } // namespace USB
